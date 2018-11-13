2665d081e6168e85bb896ad0a34aa90b44bc0234
Alex <Alex@Alexander-PC>
2018-11-13 12:32:03 +0100

    Zwerge

--- BattleArena/src/Zwerg.java
+++ BattleArena/src/Zwerg.java
@@ -1,4 +1,13 @@
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Zwerg {
-
+public class Zwerg extends Gegner{
+	
+	public Zwerg(String name, int lebenspunkte, boolean spezialfaehigkeit) {
+		super(name, lebenspunkte, spezialfaehigkeit);
+		
+	}
+	
+	public void angreifen(Gegner gegner) {
+		gegner.schadenNehmen(ThreadLocalRandom.current().nextInt(15,25 + 1));
+	} 
 }
