6fbce443b723b752d96f9b633b574816a11de461
Alex <Alex@Alexander-PC>
2018-11-13 12:55:20 +0100

    Zwerge

--- BattleArena/src/Zwerg.java
+++ BattleArena/src/Zwerg.java
@@ -7,11 +7,27 @@ public class Zwerg extends Gegner{
 	}
 	
 	public void angreifen(Gegner gegner) {
+		
 		gegner.schadenNehmen(ThreadLocalRandom.current().nextInt(15,25 + 1));
 	} 
 	
 	@Override
 	public void spezialfaehigkeitAktiviert() {
+		
+		if(super.getLebenspunkte() < 50) {
+			
+		
+		if(super.isSpezialfaehigkeit() == true) {
+			System.out.println("Spezialfähigkeit Zwergenkopfnuss aktiviert!");
+			
+		}else {
+			//nothing
+		}
+		}
+		else {
+		System.out.println("Die Spezialfaehigkeit laesst sich erst aktivieren wenn ihre Lebenspunkte unter 50 fallen!");	
+		}
+		}
 		
 	}
 }
