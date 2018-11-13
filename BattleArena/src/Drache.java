78cda8a87ca1772aff2c04ded2fc3022f82ebf41
Philipp Pali <Philipp Pali@DESKTOP-7AFB7MO>
2018-11-13 12:32:23 +0100

    angreifen

--- BattleArena/src/Drache.java
+++ BattleArena/src/Drache.java
@@ -1,9 +1,13 @@
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Drache extends Gegner{
 
 	public Drache(String name, int lebenspunkte, boolean spezialfaehigkeit) {
-		super(name, lebenspunkte, spezialfaehigkeit);
-		
+		super(name, lebenspunkte, spezialfaehigkeit);	
+	}
+	
+	public void angreifen(Gegner gegner) {
+		gegner.schadenNehmen(ThreadLocalRandom.current().nextInt(20, 25 + 1));
 	}
 }
 
