6e8786f876a997a1573f00dc43a0fc5cf586382a
Philipp Pali <Philipp Pali@DESKTOP-7AFB7MO>
2018-11-15 09:07:38 +0100

    slksagj

--- BattleArena/src/App.java
+++ BattleArena/src/App.java
@@ -3,6 +3,6 @@ public class App {
 
 	public static void main(String[] args) {
 		Kommandozeilenmenü kz = new Kommandozeilenmenü();
-		kz.start();
+		kz.setup();
 	}
 }
--- BattleArena/src/Kommandozeilenmenü.java
+++ BattleArena/src/Kommandozeilenmenü.java
@@ -4,16 +4,70 @@ import java.util.Scanner;
 public class Kommandozeilenmenü {
 
 	Scanner scan;
+	Charakter c1;
+	Charakter c2;
 
 	public Kommandozeilenmenü() {
 		this.scan = new Scanner(System.in);
+		charaktere();
+	}
+	
+	public void charaktere() {
+		c1 = new Drache("Schönwieser Drache", 500, false);
+		c2 = new Drache("Karröstner Zwerg", 500, false);
 	}
 
+	public void setupMenü() {
+		System.out.println("------------------------------");
+		System.out.println("SETUPMENÜ");
+		System.out.println("1) CHARAKTER 1");
+		System.out.println("2) CHARAKTER 2");
+		System.out.println("3) SPIEL STARTEN");
+		System.out.println("4) exit");
+	}
+	
 	public void MenüAnzeigen() {
-		System.out.println("MENÜ");
-		System.out.println("1) SETUP");
-		System.out.println("2) RECHNEN");
-		System.out.println("3) exit");
+		System.out.println("------------------------------");
+		System.out.println("SPIELMENÜ");
+		System.out.println("1) ANGREIFEN");
+		System.out.println("2) AKTIVIEREN");
+		System.out.println("3) DEAKTIVIEREN");
+		System.out.println("4) exit");
+	}
+	
+	public void setup() {
+		String s = "-";
+		while (!s.equals("2")) {
+			setupMenü();
+			s = scan.nextLine();
+			switch(s)
+			{
+			case "1":
+				this.eins();
+				break;
+			case "2":
+				this.zwei();
+				break;
+			case "3":
+				this.start();
+				break;
+			case "4":
+				System.out.println("Auf Wiedersehen!");
+				break;
+			default:
+				this.fehler();
+				break;
+			}
+		}
+		scan.close();
+	}
+	
+	public void eins() {
+		
+	}
+	
+	public void zwei() {
+		
 	}
 
 	public void start() {
@@ -27,9 +81,13 @@ public class Kommandozeilenmenü {
 				this.setup();
 				break;
 			case "2":
-				this.rechnen();
+				this.angreifen();
 				break;
 			case "3":
+				this.aktivieren();
+				break;
+			case "4":
+				this.deaktivieren();
 				break;
 			default:
 				this.fehler();
@@ -39,21 +97,26 @@ public class Kommandozeilenmenü {
 		scan.close();
 	}
 	
-	public void setup()
-	{
+	public void auswahlCharakter() {
 		System.out.println("------------------------------");
-		
-
+		System.out.println("Welchen Charakter möchten sie spielen");
 	}
 	
-	public void rechnen()
-	{
-		System.out.println("RECHNEN 1+1 = 2");
+	public void angreifen() {
+		System.out.println("------------------------------");
 	}
 	
-	public void fehler()
-	{
-		System.out.println("Sie können nur 1,2 oder 3 eingeben!");
+	public void aktivieren() {
+		System.out.println("------------------------------");
+	}
+	
+	public void deaktivieren() {
+		System.out.println("------------------------------");
+	}
+	
+	public void fehler() {
+		System.out.println("------------------------------");
+		System.out.println("Sie können nur einen der gültigen Werte eingeben!");
 	}
 
 
